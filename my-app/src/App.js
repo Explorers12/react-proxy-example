7c4d310e42b466ad686d549a5c1dc79225340bf2
Explorers12 <gva05134@naver.com>
2023-08-02 15:22:58 +0900

    일단

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,102 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BookTable from './components/BookTable';
-import DisplayBoard from './components/DisplayBoard';
-import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import BookTable from "./components/BookTable";
+import TodoTable from "./components/TodoTable";
+import DisplayBoard from "./components/DisplayBoard";
+import CreateBook from "./components/CreateBook";
+import CreateTodo from "./components/CreateTodo";
+import {
+  getAllBooks,
+  createBook,
+  createTodos,
+  getAllTodos,
+} from "./services/BookService";
+import Footer from "./components/Footer";
 
-function App () {
-
+function App() {
   const [bookShelf, setBookShelf] = useState({});
+  const [myTodo, setMyTodo] = useState({});
   const [books, setBooks] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const [numberOfTodos, setNumberOfTodos] = useState(0);
 
   const handleSubmit = () => {
-      createBook(bookShelf)
-        .then(() => {
-          setNumberBooks(numberOfBooks+1);
-      });
-  }
+    createBook(bookShelf).then(() => {
+      setNumberBooks(numberOfBooks + 1);
+    });
+  };
+
+  const handleTodoSubmit = () => {
+    createTodos(myTodo).then(() => {
+      setNumberOfTodos(numberOfTodos + 1);
+    });
+  };
 
   const getAllBook = () => {
-    getAllBooks()
-      .then(data => {
-        setBooks(data);
-        setNumberBooks(data.length);
-      });
-  }
+    getAllBooks().then((data) => {
+      setBooks(data);
+      setNumberBooks(data.length);
+    });
+  };
+
+  const getAllTodo = () => {
+    getAllTodos().then((data) => {
+      console.log(data);
+      setTodos(data);
+      setNumberOfTodos(data.length);
+    });
+  };
 
   const handleOnChangeForm = (e) => {
-      let inputData = bookShelf;
-      if (e.target.name === 'book') {
-        bookShelf.book = e.target.value;
-      } else if (e.target.name === 'category') {
-        bookShelf.category = e.target.value;
-      } else if (e.target.name === 'author') {
-        bookShelf.author = e.target.value;
-      }
-      setBookShelf(inputData);
-  }
+    let inputData = bookShelf;
+    if (e.target.name === "book") {
+      bookShelf.book = e.target.value;
+    } else if (e.target.name === "category") {
+      bookShelf.category = e.target.value;
+    } else if (e.target.name === "author") {
+      bookShelf.author = e.target.value;
+    }
+    setBookShelf(inputData);
+  };
 
-  
+  const handleOnChangeTodoForm = (e) => {
+    let inputData = myTodo;
+    if (e.target.name === "todo") {
+      myTodo.todo = e.target.value;
+    } else if (e.target.name === "category") {
+      myTodo.category = e.target.value;
+    } else if (e.target.name === "isComplete") {
+      myTodo.isComplete = e.target.value;
+    }
+    setMyTodo(inputData);
+  };
+
   return (
     <div className="main-wrapper">
       <div className="main">
         <Header />
-        <CreateBook 
-          bookShelf={bookShelf}
-          onChangeForm={handleOnChangeForm}
-          handleSubmit={handleSubmit}
-        />
-        <DisplayBoard 
-          numberOfBooks={numberOfBooks} 
-          getAllBook={getAllBook} 
+        <div className="handel_form">
+          <CreateBook
+            bookShelf={bookShelf}
+            onChangeForm={handleOnChangeForm}
+            handleSubmit={handleSubmit}
+          />
+          <CreateTodo
+            myTodo={myTodo}
+            onChangeForm={handleOnChangeTodoForm}
+            handleSubmit={handleTodoSubmit}
+          />
+        </div>
+        <DisplayBoard
+          numberOfBooks={numberOfBooks}
+          getAllBook={getAllBook}
+          getAllTodo={getAllTodo}
+          numberOfTodos={numberOfTodos}
         />
         <BookTable books={books} />
+        <TodoTable todos={todos} />
         <Footer />
       </div>
     </div>
--- my-app/src/components/DisplayBoard.js
+++ my-app/src/components/DisplayBoard.js
@@ -1,20 +1,29 @@
-const DisplayBoard = ({numberOfBooks, getAllBook}) => {
-    
-    return(
-        <div className="display-wrapper">
-            <div className="display-box">
-                <div className="display-board">
-                    <h4>생성된 수</h4>
-                    <div className="number">
-                    {numberOfBooks}
-                    </div>
-                </div>
-                <div className="get-button">
-                    <button onClick={() => getAllBook()}>Get all Books</button>
-                </div>
-            </div>
+import React from "react";
+
+const DisplayBoard = ({
+  numberOfBooks,
+  getAllBook,
+  getAllTodo,
+  numberOfTodos,
+}) => {
+  return (
+    <div className="display-wrapper">
+      <div className="display-box">
+        <div className="display-board">
+          <h4>생성된 수</h4>
+          <div className="number">{numberOfBooks}</div>
         </div>
-    )
-}
+        <div className="display-board">
+          <h4>생성된 todo 수</h4>
+          <div className="number">{numberOfTodos}</div>
+        </div>
+        <div className="get-button">
+          <button onClick={() => getAllBook()}>Get all Books</button>
+          <button onClick={() => getAllTodo()}>Get all todos</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DisplayBoard;
